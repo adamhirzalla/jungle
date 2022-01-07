77ef319e91b5328cc32d82fef73cd6041f12ed24
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-07 17:53:43 -0500

    downcase email used to verify login
    and add flash prompts to login/logout/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,26 @@ class SessionsController < ApplicationController
 
   def create
     input = login_params
-    user = User.find_by_email(input[:email])
+    user = User.find_by_email(input[:email].downcase)
+    if user.nil?
+      flash.now[:danger] = 'User not found'
+      render :new
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(input[:password])
+    elsif user.authenticate(input[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/', flash: { success: 'Successfully logged in!' }
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      flash.now[:danger] = 'Invalid email/password'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/', flash: { warning: 'User logged out.' }
   end
 
   private
