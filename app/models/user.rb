8b210414c7f2734936522291a133f9d71961aec2
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-10 19:07:56 -0500

    add class method to authenticate login credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,18 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    # downcasing logged in email (emails are case insensitive)
+    # and getting rid of spaces in email 
+    email = email.split.first.downcase
+    user = User.find_by_email(email)
+    if user.nil? || !user.authenticate(password)
+      nil
+    else
+      user
+    end
+  end
 
 end
